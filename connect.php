0c442424aaabf31493f33bc14c5a9e00d83ccb1a
aditi0206 <aditi.mishra_cs19@gla.ac.in>
2021-11-22 01:08:18 -0800

    fourth comit

--- connect.php
+++ connect.php
@@ -2,16 +2,13 @@
 $connect = mysqli_connect("localhost","root","","registration");
 if(isset($_POST['register'])){
     
-    $name = $_POST['name'];
     $roll = $_POST['roll'];
-    $date = $_POST['date'];
-    $phno = $_POST['phno'];
-    $emailid = $_POST['emailid'];
+    $Uname = $_POST['Uname'];
     $pass = $_POST['pass'];
     
     // $register = $_POST['register'];
 
-    $query = "INSERT INTO users(pin,uname,email,phoneno,passwd,surname,gender) VALUES('$name','$roll','$date','$phno','$emailid','$pass')";
+    $query = "INSERT INTO users(pin,uname,email,phoneno,passwd,surname,gender) VALUES('$roll','$Uname','$pass')";
 
     $result = mysqli_query($connect,$query);
 
