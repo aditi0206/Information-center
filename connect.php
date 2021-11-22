68d115804f8a76cef0c4de5836f1f766652d0008
aditi0206 <aditi.mishra_cs19@gla.ac.in>
2021-11-22 00:56:24 -0800

    second comit

---
+++ connect.php
@@ -0,0 +1,26 @@
+<?php
+$connect = mysqli_connect("localhost","root","","registration");
+if(isset($_POST['register'])){
+    
+    $name = $_POST['name'];
+    $roll = $_POST['roll'];
+    $date = $_POST['date'];
+    $phno = $_POST['phno'];
+    $emailid = $_POST['emailid'];
+    $pass = $_POST['pass'];
+    
+    // $register = $_POST['register'];
+
+    $query = "INSERT INTO users(pin,uname,email,phoneno,passwd,surname,gender) VALUES('$name','$roll','$date','$phno','$emailid','$pass')";
+
+    $result = mysqli_query($connect,$query);
+
+    if($result){
+        echo "registered";
+
+    }else{
+        echo "not registered";
+    }
+
+}
+?>
